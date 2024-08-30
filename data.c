665a3063adab00dced0edbbbbea44d0d69fd80f2
Hajar Hafdi <hajarlakbir@outlook.com>
2024-08-30 10:26:29 -0700

    fixed data.c

--- data.c
+++ data.c
@@ -18,28 +18,28 @@ void init_shedata(shell_info_t *sdata)
  * @sdata: ptr to the shell_info_t strct
  * @argvec: arg vector
  */
-void config_data(shell_info_t *sdata, char *argvec)
+void config_data(shell_info_t *sdata, char **argvec)
 {
 	int u = 0;
 
 	sdata->program_name = argvec[0];
 	if (sdata->input)
 	{
-		sdata->args = strtow(sdata->input, " \t");
+		sdata->args = split_string(sdata->input, " \t");
 		if (!sdata->args)
 		{
 			sdata->args = malloc(sizeof(char *) * 2);
 			if (sdata->args)
 			{
-				sdata->args[0] = _strdup(sdata->input);
+				sdata->args[0] = _strd_up(sdata->input);
 				sdata->args[1] = NULL;
 			}
 		}
 		for (u = 0; sdata->args && sdata->args[u]; u++)
 			;
 		sdata->arg_count = u;
-		replace_alias(sdata);
-		replace_vars(sdata);
+		change_aliases(sdata);
+		change_vars(sdata);
 	}
 }
 /**
@@ -50,7 +50,7 @@ void config_data(shell_info_t *sdata, char *argvec)
  */
 void release_shdata(shell_info_t *shell_info, int freeall)
 {
-	free_args(shell_info->args);
+	f_free(shell_info->args);
 	shell_info->args = NULL;
 	shell_info->current_path = NULL;
 
@@ -59,16 +59,16 @@ void release_shdata(shell_info_t *shell_info, int freeall)
 		if (!shell_info->buffer)
 			free(shell_info->input);
 		if (shell_info->env_list)
-			free_list(&(shell_info->env_list));
+			open_list(&(shell_info->env_list));
 		if (shell_info->cmd_history)
-			free_list(&(shell_info->cmd_history));
+			open_list(&(shell_info->cmd_history));
 		if (shell_info->aliases)
-			free_list(&(shell_info->aliases));
-		free_args(shell_info->env_array);
+			open_list(&(shell_info->aliases));
+		f_free(shell_info->env_array);
 		shell_info->env_array = NULL;
-		free_buffer((void **)shell_info->buffer);
+		b_free((void **)shell_info->buffer);
 		if (shell_info->input_fd > 2)
 			close(shell_info->input_fd);
-		flush_output(BUF_FLUSH);
+		_put_filed("", FLUSH_BUFFER);
 	}
 }
